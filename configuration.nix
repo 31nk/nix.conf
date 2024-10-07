da203aaa5d48632aff92fd31f4044378dab30fa9
31nk <94924779+31nk@users.noreply.github.com>
2024-10-06 21:41:42 -0400

    Add files via upload

--- configuration.nix
+++ configuration.nix
@@ -131,6 +131,7 @@
 wineWowPackages.waylandFull
 fish
 discord-screenaudio
+git
 
 
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
